eca879e9a8ac325110da2a869ed5f8e6a7e50f19
Matt Farmer <mrfarmer777@gmail.com>
2018-07-02 05:33:53 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,21 +5,20 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
-  get '/figures/new' do
-    binding.pry
+  get '/figures/new' do    
     erb :'figures/new'
   end
 
   post '/figures' do
 
     @figure=Figure.create(params[:figure])
-    if !params["title"]["name"]==[]
+    if !params["title"]["name"]==
       binding.pry
       @title=Title.find_or_create_by(params["title"])
       @figure.titles<<@title
       @title.save
     end
-    if !params['landmark']["name"]==[]
+    if !params['landmark']["name"]=={}
       @landmark=Landmark.find_or_create_by(params["landmark"])
       @figure.landmarks<<@landmark
       @landmark.save
