238c13bdb1bf0eabbad987daff76e81567c39f3f
Matt Farmer <mrfarmer777@gmail.com>
2018-07-02 04:03:22 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,7 +12,7 @@ class FiguresController < ApplicationController
     @titles=params["figure"]["titles"].collect{|id|Title.find(id)}
     @landmarks=params["figure"]["landmarks"].collect{|id|Landmark.find(id)}
     #?Why's it gotta be a hash here? vvv
-    @figure=Figure.create(name:params["figure"]["name"],titles:@titles, landmarks:@landmarks)
+    @figure=Figure.create(name:params["figure"]["name"])
     @figure.landmarks=@landmarks
     @figure.titles=@titles
     @figure.save
