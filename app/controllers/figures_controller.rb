4fdfe4f4832eb3bca47754d5c5bec12487d611ec
Matt Farmer <mrfarmer777@gmail.com>
2018-07-02 03:25:52 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,30 @@
 class FiguresController < ApplicationController
+  get '/figures' do
+    @figures=Figure.all
+    erb :'figures/index'
+  end
+
+  get 'figuress/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    #?Why's it gotta be a hash here? vvv
+    @figure=figure.create(name:params["figure"]["name"])
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure=figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure=figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+
 
 end
