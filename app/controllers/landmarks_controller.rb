3ffa24070df6300f2d4db82a0626b36d1ca9e7a7
Matt Farmer <mrfarmer777@gmail.com>
2018-07-02 02:36:05 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks=Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark=Landmark.create(params)
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark=Landmark.find(params[:id])
+    erb :'landmarks/view'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark=Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+
+
+
 end
